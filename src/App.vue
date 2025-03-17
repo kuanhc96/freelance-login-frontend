7a4714cd77d4f1664ad4676f7b14203b9a96793a
Kuan <chenkuansk@ad.garmin.com>
2025-03-17 22:18:17 +0800

    add draft for BaseContainer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
+  <base-card>
+    <base-container>Next Course</base-container>
+    <base-container></base-container>
+  </base-card>
   <div id="app">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import BaseContainer from './components/BaseContainer.vue';
+import BaseCard from './components/BaseCard.vue';
 export default {
   name: 'App',
+  components: {
+    BaseContainer,
+    BaseCard
+  }
 };
 </script>
 
---
+++ src/components/BaseContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+div {
+  max-width: 7rem;
+  max-height: 7rem;
+  margin: 2rem auto;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  background-color: #F4A7B9; /* Lemonade Pink */
+}
+
+h2 {
+  text-align: center;
+}
+</style>
