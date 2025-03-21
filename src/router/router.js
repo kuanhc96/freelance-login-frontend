ec2f003d4aa4d5b923efe5967c1f8c07c8b76413
Kuan <chenkuansk@ad.garmin.com>
2025-03-21 17:27:14 +0800

    add pages to router

--- src/router/router.js
+++ src/router/router.js
@@ -4,14 +4,31 @@ import LoginForm from '../components/LoginForm.vue';
 import CreateAccountForm from '../components/CreateAccountForm.vue';
 import ForgetPasswordForm from '../components/ForgetPasswordForm.vue';
 import ResetPasswordForm from '../components/ResetPasswordForm.vue';
-import TheDashboard from '../components/TheDashboard.vue';
+
+import InstructorsList from '../pages/instructors/InstructorList'
+import InstructorDetails from '../pages/instructors/InstructorDetails'
+import CoursesList from '../pages/courses/CoursesList'
+import CourseDetails from '../pages/courses/CourseDetails'
+import TransactionsList from '../pages/transactions/TransactionsList'
+import TransactionDetails from '../pages/transactions/TransactionDetails'
 
 const routes = [
   { path: '/login', component: LoginForm },
   { path: '/createAccount', component: CreateAccountForm },
   { path: '/forgetPassword', component: ForgetPasswordForm },
   { path: '/resetPassword', component: ResetPasswordForm },
-  { path: '/dashboard', component: TheDashboard },
+
+  { path: '/', redirect: '/dashboard' },
+  { path: '/dashboard', component: null },
+  { path: '/instructors', component: InstructorsList },
+  { path: '/instructors/:id', component: InstructorDetails, children: [
+      { path: 'contact', component: null} // /coaches/c1/contact
+  ]},
+  { path: '/courses', component: CoursesList },
+  { path: '/courses/:id', component: CourseDetails },
+  { path: '/transactions', component: TransactionsList },
+  { path: '/transactions/:id', component: TransactionDetails },
+  { path: '/:notFound(.*)', component: null }
 ];
 
 const router = createRouter({
