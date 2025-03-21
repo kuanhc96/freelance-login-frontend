ec21bba7e3c085621b6331532b6e30fb19f67ec7
Kuan <chenkuansk@ad.garmin.com>
2025-03-21 18:02:22 +0800

    add TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
+    <the-header></the-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/layout/TheHeader.vue'
 export default {
   name: 'App',
+  components: {
+    TheHeader
+  }
 };
 </script>
 
 <style>
-#app {
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0
+}
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,81 @@
+<template>
+<header>
+    <nav>
+        <h1>
+            <router-link to="/">Freelancer App</router-link> 
+        </h1>
+        <ul>
+            <li>
+                <router-link to="/instructors">Instructors</router-link> 
+            </li>
+            <li>
+                <router-link to="/courses">Courses</router-link> 
+            </li>
+            <li>
+                <router-link to="/transactions">Transaction History</router-link> 
+            </li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #b6f0a7; /* Sage Green */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header a {
+  text-decoration: none;
+  color: #e677d3;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #e677d3;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+</style>
