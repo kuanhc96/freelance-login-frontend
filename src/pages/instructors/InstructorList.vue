1a24976e2ab303a1270760efb1c9a4e3517e9b91
Kuan <chenkuansk@ad.garmin.com>
2025-03-23 00:08:54 +0800

    add unordered list in instructor list

--- src/pages/instructors/InstructorList.vue
+++ src/pages/instructors/InstructorList.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Test</h1>
+    <h1>Instructors</h1>
+    <ul>
+        <li>
+            <base-summary>
+                <h3>Alice Ho</h3> 
+            </base-summary>
+        </li>
+    </ul>
 </template>
+
+<script>
+import BaseSummary from '../../components/BaseSummary.vue'
+export default {
+    components: {
+        BaseSummary
+    }
+    
+}
+</script>
