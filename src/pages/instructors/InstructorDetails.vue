102be90d2167fba940aebef403c95bfc0fd1d809
Kuan <chenkuansk@ad.garmin.com>
2025-03-24 18:12:41 +0800

    add details to InstructorDetails page

--- src/pages/instructors/InstructorDetails.vue
+++ src/pages/instructors/InstructorDetails.vue
@@ -1,5 +1,51 @@
 <template>
     <h1>InstructorDetails</h1>
-    <router-view></router-view>
-    <router-link to="/instructors/c1/contact">Contact</router-link>
+    <section>
+        <base-card>
+            <h2>{{instructorName}} </h2> 
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+            <header>
+                <h2> Interested? Reach out now! </h2> 
+                <base-button link :to="contactLink">Contact</base-button>
+            </header>
+            <router-view/>
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+            <base-badge v-for="subject in filteredSubjects" 
+                :key="subject.subjectName"
+                :type="subject.subjectName"
+                :title="subject.subjectName"
+            >{{subject.subjectName}}</base-badge>
+            <p>{{ description }}</p> 
+        </base-card>
+    </section>
 </template>
+
+<script>
+export default {
+    props: ['id'],
+    data() {
+        return {
+            selectedInstructor: null,
+            instructorName: '',
+            description: 'The best golf coach ever!'
+        };
+    },
+    computed: {
+        filteredSubjects() {
+            return this.$store.getters['subjects/subjects'];
+        }
+    },
+    created() {
+        this.selectedInstructor = this.$store.getters['instructors/instructors'].find(
+            (instructor) => instructor.id === this.id
+        );
+        this.instructorName = this.selectedInstructor.instructorName;
+    }
+};
+</script>
