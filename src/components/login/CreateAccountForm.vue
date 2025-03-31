5c69c951fe5469ffb38b22fdc012fe2aa72c9b5e
Kuan <chenkuansk@ad.garmin.com>
2025-03-31 18:54:28 +0800

    add style to account creation form

--- src/components/login/CreateAccountForm.vue
+++ src/components/login/CreateAccountForm.vue
@@ -31,14 +31,16 @@
           </select>
         </div>
         <div>
-          Gender
-          <input
-                  type="radio"
-                  id="male"
-                  value="male"
-                  v-model="selectedGender"
-          />
-          <label for="male">Male</label>
+          <label>Gender</label>
+          <span>
+            <input
+                    type="radio"
+                    id="male"
+                    value="male"
+                    v-model="selectedGender"
+            />
+            <label for="male">Male</label>
+          </span>
           <input
                   type="radio"
                   id="female"
@@ -82,6 +84,86 @@
     </BaseCard>
   </section>
 </template>
+
+<style scoped>
+
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input[type='radio'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='radio'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+
+</style>
+
 <script>
 export default {
   data() {
@@ -134,15 +216,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-</style>
