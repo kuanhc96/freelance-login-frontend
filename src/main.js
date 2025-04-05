249e0f2a298e32623be528bdd5f55b15adf50ccf
Kuan <chenkuansk@ad.garmin.com>
2025-04-05 16:01:46 +0800

    add bootstrap import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router/router.js';
 import store from './store/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
