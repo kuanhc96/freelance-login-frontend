512d391a90c2fb78e48d36de03a474e103065ccc
Kuan <chenkuansk@ad.garmin.com>
2025-04-23 14:19:06 +0800

    dashboard outline

--- src/pages/Dash-Board.vue
+++ src/pages/Dash-Board.vue
@@ -1,3 +1,47 @@
 <template>
    <h1>Dashboard</h1> 
+   <div class="row">
+      <div class="m-3">
+         <div class="card ">
+            <div class="card-body">
+               <h4 class="card-title">Announcement</h4>
+               <p class="card-text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos harum sapiente, cumque rerum minus rem saepe consequuntur at culpa molestiae.
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card ">
+            <div class="card-body">
+               <h4 class="card-title">Upcoming Lesson</h4>
+               <p class="card-text">Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card ">
+            <div class="card-body">
+               <h4 class="card-title">Most Recent Transaction</h4>
+               <p class="card-text">Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card ">
+            <div class="card-body">
+               <h4 class="card-title">Subscribed Instructors</h4>
+               <p class="card-text">Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="card ">
+            <div class="card-body">
+               <h4 class="card-title">Account Information</h4>
+               <p class="card-text">Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
