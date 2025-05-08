c0214ce203528b5a0d115ef435d318479cdb5215
Kuan <chenkuansk@ad.garmin.com>
2025-05-08 22:18:50 +0800

    move stuff under navbar

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="d-flex justify-content-center m-5">
-    <div class="card base-card bg-light col-md-10">
+  <div class="d-flex justify-content-center">
+    <div class="card base-card bg-light col-md-10 mt-7">
       <div class="card-body">
         <h2 class="card-title">{{cardTitle}}</h2>
         <slot></slot>
