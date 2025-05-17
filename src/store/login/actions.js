342e2d569acb2037eef98d99cc2df445aa1050d4
Kuan <chenkuansk@ad.garmin.com>
2025-05-17 22:15:25 +0800

    remove subscribedIds

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -1,5 +1,5 @@
 export default {
-    login({commit}, {status, userId, subscribedIds, role}) {
+    login({commit}, {status, userId, role}) {
         commit('SET_LOGIN_STATUS', status);
         commit('SET_USER_ID', userId);
         commit('SET_ROLE', role);
