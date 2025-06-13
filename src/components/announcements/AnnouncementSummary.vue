2caf5dbd820a6666432ab891816a1e2d939b8f5a
Kuan <chenkuansk@ad.garmin.com>
2025-06-13 16:33:56 +0800

    by default accordion should be closed

--- src/components/announcements/AnnouncementSummary.vue
+++ src/components/announcements/AnnouncementSummary.vue
@@ -14,7 +14,7 @@
                     </div>
                 </button>
             </h2>
-            <div class="accordion-collapse collapse show" :id="announcementId" data-bs-parent="#myAccordion">
+            <div class="accordion-collapse collapse " :id="announcementId" data-bs-parent="#myAccordion">
                 <div class="accordion-body d-flex justify-content-between align-items-center">
                     <div class="">
                         {{ announcement }}
