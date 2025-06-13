9c40e654e1dff2228d8ddc8a7424c16089bde8d5
Kuan <chenkuansk@ad.garmin.com>
2025-06-13 16:30:21 +0800

    make instructors and announcements stores persistent in store

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,19 @@ import createPersistedState from 'vuex-persistedstate';
 import instructors from './instructors';
 import subjects from './subjects';
 import login from './login';
+import announcements from './announcements'
 
 const store = createStore({
     modules: {
         instructors,
         subjects,
         login,
+        announcements,
     },
     plugins: [
         createPersistedState({
             key: 'freelance-app',
-            paths: ['login']
+            paths: ['login', 'instructors', 'announcements']
         })
     ]
 })
