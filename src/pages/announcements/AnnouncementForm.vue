2b295d0ec5858e8fe910ece894997193b278dd7e
Kuan <chenkuansk@ad.garmin.com>
2025-07-04 13:44:52 +0800

    fix formatting

--- src/pages/announcements/AnnouncementForm.vue
+++ src/pages/announcements/AnnouncementForm.vue
@@ -2,19 +2,21 @@
     <section>
         <base-card :cardTitle="'Make Announcement'">
             <div class="row">
-              <div class="col-12">
-                <form  action="">
-                  <div class="m-3">
-                    <input v-model="subject" type="text" name="subject" placeholder="Subject" class="form-control form-control-lg">
-                  </div>
-                  <div class="m-3">
-                    <textarea v-model="announcement" class="form-control form-control-lg" name="announcement" rows="6" placeholder="Announcement"></textarea>
-                  </div>
-                  <div class="d-grid">
-                    <button class="btn btn-secondary mt-3" type="submit" @click="submit">Submit</button>
-                  </div>
-                </form>
-              </div>
+                <div class="col-12">
+                    <form action="">
+                        <div class="m-3">
+                            <input v-model="subject" type="text" name="subject" placeholder="Subject"
+                                   class="form-control form-control-lg">
+                        </div>
+                        <div class="m-3">
+                            <textarea v-model="announcement" class="form-control form-control-lg" name="announcement"
+                                      rows="6" placeholder="Announcement"></textarea>
+                        </div>
+                        <div class="d-grid">
+                            <button class="btn btn-secondary mt-3" type="submit" @click="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </base-card>
     </section>
@@ -22,8 +24,9 @@
 </template>
 <script lang="ts">
 import BaseCard from '@/components/ui/BaseCard.vue';
-import { CreateAnnouncementResponse } from '@/dto/response/createAnnouncementResponse';
-import { defineComponent } from 'vue'
+import {CreateAnnouncementResponse} from '@/dto/response/createAnnouncementResponse';
+import {defineComponent} from 'vue'
+
 export default defineComponent({
     name: 'AnnouncementForm',
     components: {
@@ -47,7 +50,7 @@ export default defineComponent({
                         credentials: 'include',
                         headers: {
                             'Content-Type': 'application/json'
-                        }, 
+                        },
                         body: JSON.stringify({
                             instructorGUID: 'aa02e645-55ea-4aa3-953e-3ea543c8290f',
                             title: this.subject,
