44f68a3366fb581148808d1203f6fbf01b9ef2b4
Kuan <chenkuansk@ad.garmin.com>
2025-07-04 13:56:51 +0800

    fix formatting

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,38 +1,38 @@
 <template>
-<header>
-    <div class="navbar navbar-expand-lg navbar-dark bg-light fixed-top"
-         v-if="isLoggedIn"
-    >
-      <div class="container">
-        <router-link to="/" class="navbar-brand">
-          <img src="@/assets/images/logo.png" alt="Logo" id="logo-img" width="150"/>
-        </router-link> 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-                <router-link to="/instructors" class="nav-link">Instructors</router-link> 
-            </li>
-            <li class="nav-item">
-                <router-link to="/lessons" class="nav-link ">Lessons</router-link> 
-            </li>
-            <li class="nav-item">
-                <router-link to="/subjects" class="nav-link ">Subjects</router-link> 
-            </li>
-            <li class="nav-item">
-                <router-link to="/transactions" class="nav-link ">Transaction</router-link> 
-            </li>
-            <li class="nav-item">
-                <router-link to="/instructors/search" class="nav-link ">Search</router-link> 
-            </li>
-            <li class="nav-item">
-                <router-link to="/login?logout=true" class="nav-link ">Logout</router-link> 
-            </li>
-        </ul>
-        <span class="nav-item">
+    <header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-light fixed-top"
+             v-if="isLoggedIn"
+        >
+            <div class="container">
+                <router-link to="/" class="navbar-brand">
+                    <img src="@/assets/images/logo.png" alt="Logo" id="logo-img" width="150"/>
+                </router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <router-link to="/instructors" class="nav-link">Instructors</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/lessons" class="nav-link ">Lessons</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/subjects" class="nav-link ">Subjects</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/transactions" class="nav-link ">Transaction</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/instructors/search" class="nav-link ">Search</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/login?logout=true" class="nav-link ">Logout</router-link>
+                    </li>
+                </ul>
+                <span class="nav-item">
             <span class="fa-stack">
               <a href="#">
                 <i class="fas fa-circle fa-stack-2x"></i>
@@ -40,7 +40,7 @@
               </a>
             </span>
           </span>
-          <span class="nav-item">
+                <span class="nav-item">
             <span class="fa-stack">
               <a href="#">
                 <i class="fas fa-circle fa-stack-2x text-secondary"></i>
@@ -49,9 +49,9 @@
             </span>
           </span>
 
-      </div>
-    </div>
-</header>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script lang="ts">
@@ -59,8 +59,9 @@ import { mapGetters } from 'vuex';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  computed: {
-    ...mapGetters('login', ['isLoggedIn']),
-  }
+
+    computed: {
+        ...mapGetters('login', ['isLoggedIn']),
+    }
 })
 </script>
