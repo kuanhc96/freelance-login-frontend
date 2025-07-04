5ec5acab2b8b324efb6f71f0ff41cd3cd1306eb8
Kuan <chenkuansk@ad.garmin.com>
2025-07-04 08:06:46 +0800

    implement composition API for BaseCard

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,32 +1,37 @@
 <template>
     <div class="d-flex justify-content-center">
-      <div class="card base-card bg-light col-md-10 mt-7 rounded-4">
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <h2 class="card-title">{{cardTitle}}</h2>
-            <div class="refresh">
-                <button class="btn btn-primary" @click="refresh">Refresh</button>
+        <div class="card base-card bg-light col-md-10 mt-7 rounded-4">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <h2 class="card-title">{{ cardTitle }}</h2>
+                    <div class="refresh">
+                        <button class="btn btn-primary" @click="refresh">Refresh</button>
+                    </div>
+                </div>
+                <slot></slot>
             </div>
-          </div>
-          <slot></slot>
         </div>
-      </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import {defineComponent, PropType} from 'vue';
+
 export default defineComponent({
-  props: {
-    cardTitle: {
-      type: String as PropType<string>,
-        required: true,
-    }
-  },
-  methods: {
-    refresh(): void {
-      this.$emit('refresh');
-    }
-  }
+    props: {
+        cardTitle: {
+            type: String as PropType<string>,
+            required: true,
+        }
+    },
+    setup(_, context) {
+        function refresh() {
+            context.emit('refresh');
+        }
+
+        return {
+            refresh
+        }
+    },
 })
 </script>
