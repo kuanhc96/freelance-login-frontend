ed2beb65aa3d3f59db9ade3f4f700629876c6fbc
Kuan <chenkuansk@ad.garmin.com>
2025-07-09 16:54:13 +0800

    add lessons store

---
+++ src/dto/response/getLessonResponse.ts
@@ -0,0 +1,14 @@
+export interface GetLessonsResponse {
+    lessonGUID: string
+    studentGUID: string
+    instructorGUID: string
+    startDate: string
+    location: string
+    topic: string
+    subject: string
+    lessonStatus: string
+    discount: number
+    instructorComments: string
+    studentFeedback: string
+    lessonRating: number
+}
---
+++ src/store/lessons/index.ts
@@ -0,0 +1,32 @@
+import { GetLessonResponse } from '@/dto/response/getLessonResponse'
+import { defineStore } from 'pinia';
+
+export interface LessonsState {
+    studentGUIDToLessonsMap: Record<string, GetLessonResponse[]>,
+    instructorGUIDToLessonsMap: Record<string, GetLessonResponse[]>
+}
+
+export interface AddLessonsPayload {
+    userGUID: string,
+    lessons: GetLessonResponse[]
+}
+
+export const useLessonsStore = defineStore('lessons', {
+    state: (): LessonsState => ({
+        studentGUIDToLessonsMap: {},
+        instructorGUIDToLessonsMap: {},
+    }),
+    getters: {
+        getLessonsByStudentGUID: (state) => (studentGUID: string) => { return state.studentGUIDToLessonsMap[studentGUID]},
+        getLessonsByInstructorGUID: (state) => (instructorGUID: string) => { return state.instructorGUIDToLessonsMap[instructorGUID]},
+    },
+    actions: {
+        addLessonsByStudentGUID(payload: AddLessonsPayload) {
+            this.studentGUIDToLessonsMap[payload.userGUID] = payload.lessons;
+        },
+        addLessonsByInstructorGUID(payload: AddLessonsPayload) {
+            this.instructorGUIDToLessonsMap[payload.userGUID] = payload.lessons;
+        }
+    },
+    persist: true,
+})
