e62f65ece13723dac08bb951faea018e8004f91c
Kuan <chenkuansk@ad.garmin.com>
2025-07-12 09:28:01 +0800

    move login/logout API calls into login store

--- src/store/login/index.ts
+++ src/store/login/index.ts
@@ -1,41 +1,106 @@
 import { LoginResponse } from '@/dto/response/loginResponse';
+import { LoginRequest } from '@/dto/request/loginRequest';
 import { defineStore } from 'pinia';
+import Cookies from "js-cookie";
+import router from "@/router/router";
 
 export interface LoginState {
-    loginResponse: LoginResponse
+    xsrfToken: string
+    userGUID: string
+    email: string
+    role: string
     backendService: string
 }
 
 export const useLoginStore = defineStore('login', {
     state: (): LoginState => ({
-        loginResponse: {
-            success: false,
-            userGUID: '',
-            email: '',
-            role: ''
-        },
+        xsrfToken: Cookies.get('XSRF-TOKEN') || '',
+        userGUID: localStorage.getItem('userGUID') || '',
+        email: '',
+        role: '',
         backendService: 'http://localhost:8081/'
     }),
     getters: {
-        isLoggedIn: state => state.loginResponse.success,
-        getUserGUID: state => state.loginResponse.userGUID,
-        getRole: state => state.loginResponse.role,
-        getEmail: state => state.loginResponse.email,
+        isLoggedIn: (state) => {
+            console.log("LOL", state.xsrfToken);
+            console.log("LOL", state.userGUID);
+            return !!state.userGUID?.trim() && !!state.xsrfToken?.trim()
+        },
+        getXsrfToken: state => state.xsrfToken,
+        getUserGUID: state => state.userGUID,
+        getRole: state => state.role,
+        isStudent: state => state.role === 'STUDENT',
+        getEmail: state => state.email,
         getBackendService: state => state.backendService
     },
     actions: {
-        login(payload: LoginResponse) {
-            this.loginResponse.success = payload.success;
-            this.loginResponse.userGUID = payload.userGUID;
-            this.loginResponse.role = payload.role;
-            this.loginResponse.email = payload.email;
+        async checkLogin(): Promise<void> {
+            if (this.isLoggedIn) {
+                const response = await fetch(
+                    'http://localhost:8081/checkLogin', {
+                        method: 'POST',
+                        credentials: 'include'
+                    }
+                );
+
+                if (response.ok) {
+                    const data: LoginResponse = await response.json();
+                    if (data) {
+                        this.userGUID = data.userGUID;
+                        localStorage.setItem('userGUID', data.userGUID);
+                        this.xsrfToken = Cookies.get('XSRF-TOKEN');
+                        this.role = data.role;
+                        this.email = data.email;
+                        await router.replace('/')
+                    }
+                } else {
+                    throw new Error('Not authenticated')
+                }
+            } else {
+                await router.replace('/login');
+            }
+
         },
-        logout() {
-            this.loginResponse.success = false;
-            this.loginResponse.userGUID = '';
-            this.loginResponse.role = '';
-            this.loginResponse.email = '';
+        async login(loginRequest: LoginRequest): Promise<void> {
+            const response: Response = await fetch("http://localhost:8081/apiLogin", {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'email': loginRequest.email,
+                    'password': loginRequest.password,
+                    'role': loginRequest.role,
+                }),
+            });
+
+            if (response.ok) {
+                const data: LoginResponse = await response.json();
+                console.log(data);
+                if (data) {
+                    this.xsrfToken = Cookies.get('XSRF-TOKEN');
+                    this.userGUID = data.userGUID;
+                    localStorage.setItem('userGUID', data.userGUID);
+                    this.role = data.role;
+                    this.email = data.email;
+                    // localStorage.setItem('expirationTimestamp', new Date().getTime() + data.tokenDuration)
+                    await router.replace('/')
+                }
+            } else {
+                console.log(response);
+            }
+        },
+        async logout(): Promise<void> {
+            await fetch('http://localhost:8081/apiLogout', {
+                method: 'POST',
+                credentials: 'include'
+            })
+            this.xsrfToken = '';
+            this.userGUID = '';
+            localStorage.removeItem('userGUID');
+            this.role = '';
+            this.email = '';
         }
     },
-    persist: true,
 })
