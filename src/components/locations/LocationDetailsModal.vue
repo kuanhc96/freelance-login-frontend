0c96a5d13bd503b52b0baee383c0841546f4c696
Kuan <chenkuansk@ad.garmin.com>
2025-07-30 10:56:53 +0800

    LocationDetailsModal component

---
+++ src/components/locations/LocationDetailsModal.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+import {GetLocationResponse} from "@/dto/response/getLocationResponse";
+
+export default defineComponent({
+    name: 'LocationDetailsModal',
+    props: {
+        location: {
+            type: Object as PropType<GetLocationResponse>,
+            required: true
+        }
+    },
+})
+</script>
+
+<template>
+    <span
+        class="badge btn text-bg-secondary text-white fs-7 mx-2"
+        :data-bs-target="'#modal' + location.locationGUID"
+        data-bs-toggle="modal"
+    >
+        {{location.locationName}}
+    </span>
+    <div class="modal fade" :id="'modal'+location.locationGUID">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">{{location.locationName}}</h1>
+                    <button class="btn btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    {{location.street}}, {{location.city}}, {{location.country}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/pages/profile/ProfilePage.vue
+++ src/pages/profile/ProfilePage.vue
@@ -1,7 +1,23 @@
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {computed, defineComponent, Ref} from 'vue'
+import {GetLocationResponse} from "@/dto/response/getLocationResponse";
+import {useLocationsStore} from "@/store/locations";
+import LocationDetailsModal from "@/components/locations/LocationDetailsModal.vue";
 export default defineComponent({
     name: 'ProfilePage',
+    components: {
+        LocationDetailsModal
+    },
+    setup() {
+        const locationsStore = useLocationsStore();
+        const preferredLocations: Ref<GetLocationResponse[]> = computed(function() {
+            return locationsStore.getLocations;
+        })
+
+        return {
+            preferredLocations
+        }
+    }
 })
 
 </script>
@@ -10,7 +26,27 @@ export default defineComponent({
     <section>
         <base-card
             card-title="Profile"
-        ></base-card>
+        >
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="row col-md-12 col-12 m-3">
+                    <div class="d-flex justify-content-between">
+                        <h5>Preferred Locations:</h5>
+                        <div class="">
+<!--                            modal button for location details -->
+                            <location-details-modal
+                                v-for="location in preferredLocations"
+                                :key="location.locationGUID"
+                                :location="location"
+                            > </location-details-modal>
+                            <span v-if="preferredLocations.length > 2" class="fs-6"> ... </span>
+                            <span class="badge btn text-bg-primary text-dark fs-7 mx-2">
+                                + Add
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </base-card>
     </section>
 </template>
 
