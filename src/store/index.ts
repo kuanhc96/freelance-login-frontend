f99efda6f023f25024cda7b4fb250337684c8fc9
Kuan <chenkuansk@ad.garmin.com>
2026-01-21 11:15:07 +0800

    adjust endpoint paths

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,18 +9,17 @@ pinia.use(({ store }) => {
 
 export default pinia;
 
-export const FREELANCE_RESOURCE_BACKEND: string = 'http://localhost:8072/freelance-resource-backend/freelance'
+export const FREELANCE_RESOURCE_BACKEND: string = 'http://localhost:8072/freelance'
 export const FREELANCE_AUTHSERVER: string = 'http://localhost:8072/freelance-authserver'
-export const USERS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + '/users';
-export const SUBSCRIPTION_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND +'/subscriptions';
-export const SUBSCRIBED_INSTRUCTORS_ENDPOINT: string = SUBSCRIPTION_ENDPOINT + '/instructors';
+export const USERS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + '/user';
+export const SUBSCRIPTION_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND +'/subscription';
+export const SUBSCRIBED_INSTRUCTORS_ENDPOINT: string = SUBSCRIPTION_ENDPOINT + '/instructor';
 export const UNSUBSCRIBED_INSTRUCTORS_ENDPOINT: string = SUBSCRIPTION_ENDPOINT + '/unsubscribed';
-export const SUBSCRIBED_STUDENTS_ENDPOINT: string = SUBSCRIPTION_ENDPOINT + '/students';
-export const ANNOUNCEMENTS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/announcements";
-export const SUBJECTS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/subjects";
-export const LESSONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/lessons";
-export const PACKAGES_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/packages";
-export const PACKAGES_BY_SUBJECT_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + PACKAGES_ENDPOINT + "/subjects";
-export const TRANSACTIONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/transactions";
-export const LOCATIONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/locations";
-export const TOKEN_ENDPOINT: string = FREELANCE_AUTHSERVER + "/oauth/tokens";
+export const SUBSCRIBED_STUDENTS_ENDPOINT: string = SUBSCRIPTION_ENDPOINT + '/student';
+export const ANNOUNCEMENTS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/announcement";
+export const SUBJECTS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/subject";
+export const LESSONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/lesson";
+export const PACKAGES_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/package";
+export const PACKAGES_BY_SUBJECT_ENDPOINT: string =  PACKAGES_ENDPOINT + "/subject";
+export const TRANSACTIONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/transaction";
+export const LOCATIONS_ENDPOINT: string = FREELANCE_RESOURCE_BACKEND + "/location";
