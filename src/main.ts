1b0ca3192b06df2026b8368525ee8bb922979ec0
Kuan <chenkuansk@ad.garmin.com>
2026-03-17 15:09:38 +0800

    delete comments

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,8 @@ import '@/assets/scss/styles.scss'
 import '@/assets/scss/font-awesome.scss'
 import BaseCard from './components/ui/BaseCard.vue';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
-// import {createOAuthClient, SessionStorage} from "@volverjs/auth-vue";
 
 const app = createApp(App);
-// const authClient = createOAuthClient({
-//     url: AUTH_SERVER_LOCATION,
-//     clientId: 'fe-public-client',
-//     tokenEndpointAuthMethod: 'none',
-//     scopes: 'openid FREELANCE_FE',
-//     storage: new SessionStorage('freelance-session-storage'),
-//     redirectUri: CALLBACK_URL,
-//     postLogoutRedirectUri: AUTH_SERVER_LOCATION + '/login?autoLoggedOut=true',
-// });
-// app.use(authClient, {global: true});
 app.use(router);
 app.use(pinia);
 app.component('base-card', BaseCard);
--- src/pages/login/CallbackPage.vue
+++ src/pages/login/CallbackPage.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-// import {useOAuthClient} from "@volverjs/auth-vue";
 import {onBeforeMount} from "vue";
-// import router from "@/router/router";
 
-// const authClient = useOAuthClient();
 onBeforeMount(async () => {
-    // authClient.handleCodeResponse(new URLSearchParams(urlParams));
-    // router.replace("/");
     const urlString: string = window.location.search;
-    // const authCode = urlParams
     const response: Response = await fetch(
         'http://localhost:8083/api/oauth/tokens' + urlString, {
             method: 'POST',
@@ -31,7 +25,3 @@ onBeforeMount(async () => {
 <template>
     <div>Redirecting...</div>
 </template>
-
-<style scoped>
-
-</style>
