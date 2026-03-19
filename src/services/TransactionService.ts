596e9f5cce9b302f08cf7d05341453e5c01a84d1
Kuan <chenkuansk@ad.garmin.com>
2026-03-19 16:08:07 +0800

    implement transaction service

---
+++ src/services/TransactionService.ts
@@ -0,0 +1,17 @@
+import apiClient from "@/services/apiClient";
+import {GetTransactionResponse} from "@/dto/response/getTransactionResponse";
+import {useLoginStore} from "@/store/login";
+import {TRANSACTIONS_ENDPOINT} from "@/store";
+
+export default {
+    getTransactionsByUserGUID(userGUID: string) {
+        const loginStore = useLoginStore();
+        let transactionsEndpoint = TRANSACTIONS_ENDPOINT;
+        if (loginStore.isStudent) {
+            transactionsEndpoint += '/student' + '/' + userGUID;
+        } else {
+            transactionsEndpoint += '/instructor' + '/' + userGUID;
+        }
+        return apiClient.get<GetTransactionResponse[]>(transactionsEndpoint);
+    }
+}
--- src/store/transactions/index.ts
+++ src/store/transactions/index.ts
@@ -1,7 +1,7 @@
 import { GetTransactionResponse } from '@/dto/response/getTransactionResponse';
 import { defineStore } from 'pinia';
 import {useLoginStore} from "@/store/login";
-import {TRANSACTIONS_ENDPOINT} from "@/store";
+import TransactionService from "@/services/TransactionService";
 
 export interface TransactionsState {
     transactions: GetTransactionResponse[]
@@ -19,20 +19,10 @@ export const useTransactionsStore = defineStore('transactions', {
     actions: {
         async setTransactions() {
             const loginStore = useLoginStore();
-            let transactionsEndpoint = TRANSACTIONS_ENDPOINT;
-            if (loginStore.isStudent) {
-                transactionsEndpoint += '/student' + '/' + loginStore.getUserGUID;
-            } else {
-                transactionsEndpoint += '/instructor' + '/' + loginStore.getUserGUID;
-            }
-            const response: Response = await fetch(transactionsEndpoint, {
-                method: 'GET',
-                credentials: 'include'
-            })
-            if (response.ok) {
-
-                this.transactions = await response.json();
-            }
+            TransactionService.getTransactionsByUserGUID(loginStore.getUserGUID)
+                .then((res) => {
+                     this.transactions = res.data;
+                });
         }
     }
 })
